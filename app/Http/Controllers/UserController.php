9426db5bf30f6f4c8c0259bbab4b29656df5d326
sam cross <sam.cross@gmx.com>
2024-02-20 23:12:09 +0000

    added user

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,15 @@
+<?php
+namespace App\Http\Controllers;
+use App\Models\User;
+
+use Inertia\Inertia;
+
+class UserController extends Controller
+{
+    public function show(User $user)
+    {
+        return Inertia::render('User/Show', [
+          'user' => $user
+        ]);
+    }
+}
