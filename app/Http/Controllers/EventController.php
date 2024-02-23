064bea71cf0d485a32c55986568a82e4dc133976
Sam Cross <scross@safetyservices.co.uk>
2024-02-23 16:37:28 +0000

    add event

---
+++ app/Http/Controllers/EventController.php
@@ -0,0 +1,86 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\StoreEventRequest;
+use App\Http\Requests\UpdateEventRequest;
+use App\Models\Event;
+use Illuminate\Http\Request;
+use Inertia\Inertia;
+
+class EventController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        $events = Event::all(); // Assuming you have an Event model
+
+        return Inertia::render('Events/Index', [
+            'events' => $events,
+        ]);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        return Inertia::render('Events/Create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreEventRequest $request)
+    {
+        $validated = $request->validated();
+
+        Event::create($validated);
+
+        return redirect()->route('events.index');
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Event $event)
+    {
+        return Inertia::render('Events/Show', [
+            'event' => $event,
+        ]);
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Event $event)
+    {
+        return Inertia::render('Events/Edit', [
+            'event' => $event,
+        ]);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateEventRequest $request, Event $event)
+    {
+        $validated = $request->validated();
+
+        $event->update($validated);
+
+        return redirect()->route('events.index');
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Event $event)
+    {
+        $event->delete();
+
+        return redirect()->route('events.index');
+    }
+}
---
+++ database/factories/EventFactory.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+use App\Models\User;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Event>
+ */
+class EventFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'name' => fake()->word(),
+            'startDate' => fake()->date(),
+            'endDate' => fake()->date(),
+            'user' => User::all()->random()->id
+        ];
+    }
+}
